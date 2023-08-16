b253f19bdab2c31b50daaf9359bded587782a45a
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-17 01:21:06 +0300

    Basic widget layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Widget from "./ui/Widget";
+
 function App() {
   return (
-    <div className="">
-      <div className=""></div>
+    <div className="h-full min-h-screen w-screen bg-sky-100">
+      <main className="mx-auto grid w-full max-w-5xl grid-cols-2 gap-4">
+        <Widget></Widget>
+        <Widget></Widget>
+        <Widget></Widget>
+        <Widget></Widget>
+      </main>
     </div>
   );
 }
