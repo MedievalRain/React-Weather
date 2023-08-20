9abddbb1e55dda261ada96879eeb35cc165c1c8a
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-20 23:12:39 +0300

    i18n

--- src/features/weather/UVWidget/UVWidget.tsx
+++ src/features/weather/UVWidget/UVWidget.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import useWeather from "../../../hooks/useWeather";
 import { normalizeDate, toTimestampInSeconds } from "../../../utils/format";
 import UvDescription from "./UvDescription";
@@ -5,14 +6,14 @@ import UvIcon from "./UvIcon";
 
 function UVWidget() {
   const weather = useWeather();
-
+  const { t } = useTranslation();
   if (weather) {
     const timestamp = toTimestampInSeconds(normalizeDate(new Date()));
     const index = weather.hourly.time.indexOf(timestamp);
     const uvIndex = Math.round(weather.hourly.uv_index[index]);
     return (
       <div className="flex flex-col items-center p-4">
-        <div className="font-semibold">УФ-индекс</div>
+        <div className="font-semibold">{t("uv.uv_index")}</div>
         <UvIcon uvIndex={uvIndex} />
         <UvDescription uvIndex={uvIndex} />
       </div>
