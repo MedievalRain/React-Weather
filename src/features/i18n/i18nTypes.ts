e194830d42ab33d0a7fcde26655f1669f94b700b
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-21 03:15:28 +0300

    Change imports to reduce bundle size

--- src/features/i18n/i18nTypes.ts
+++ src/features/i18n/i18nTypes.ts
@@ -1,5 +1,6 @@
-import { ru, enUS } from "date-fns/locale";
 import type { Locale } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+import ru from "date-fns/locale/ru";
 
 export const languageSchema: Record<string, Locale> = {
   ru: ru,
