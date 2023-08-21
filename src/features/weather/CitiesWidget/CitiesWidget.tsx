feb533afc0b5ebda836a81ae678283d21a0363c8
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-21 14:15:38 +0300

    Fetch weather

--- src/features/weather/CitiesWidget/CitiesWidget.tsx
+++ src/features/weather/CitiesWidget/CitiesWidget.tsx
@@ -10,34 +10,34 @@ const placeholderCities = [
     id: 524901,
   },
   {
-    country: "Россия",
-    latitude: 55.75222,
-    longitude: 37.61556,
+    country: "Беларусь",
+    latitude: 53.9,
+    longitude: 27.5667,
     name: "Минск",
     timezone: "Europe/Moscow",
-    id: 524901,
+    id: 521901,
   },
   {
-    country: "Россия",
-    latitude: 55.75222,
-    longitude: 37.61556,
+    country: "США",
+    latitude: 40.7143,
+    longitude: -74.006,
     name: "Нью-Йорк",
     timezone: "Europe/Moscow",
-    id: 524901,
+    id: 524323901,
   },
   {
-    country: "Россия",
-    latitude: 55.75222,
-    longitude: 37.61556,
+    country: "Франция",
+    latitude: 55.5575,
+    longitude: 58.5836,
     name: "Париж",
     timezone: "Europe/Moscow",
-    id: 524901,
+    id: 5241234123901,
   },
 ];
 
 function CitiesWidget() {
   return (
-    <div className="flex flex-col px-2 py-4">
+    <div className="flex flex-col gap-2 px-2 py-4">
       <div className="font-semibold">Погода в других городах</div>
       <div className="flex flex-wrap gap-4">
         {placeholderCities.map((city) => (
--- src/features/weather/CitiesWidget/CityChip.tsx
+++ src/features/weather/CitiesWidget/CityChip.tsx
@@ -1,3 +1,5 @@
+import { weatherApi } from "../../../services/weather";
+import WeatherIcon from "../../../ui/WeatherIcon";
 import { CityData } from "../../city/cityTypes";
 
 interface CityChipProps {
@@ -5,7 +7,29 @@ interface CityChipProps {
 }
 
 function CityChip({ city }: CityChipProps) {
-  return <div>{city.name}</div>;
+  const { latitude, longitude, timezone, id } = city;
+  const { data: weather, isFetching } = weatherApi.useGetWeatherQuery(
+    {
+      latitude,
+      longitude,
+      timezone,
+    },
+
+    { skip: id === 0 },
+  );
+
+  if (weather) {
+    return (
+      <button className="flex items-center rounded-md bg-sky-100 px-2  font-semibold transition-all duration-200  hover:bg-sky-200 hover:shadow-md">
+        {city.name}, {city.country}{" "}
+        {Math.round(weather.current_weather.temperature)}°
+        <WeatherIcon
+          size="small"
+          weathercode={weather.current_weather.weathercode}
+        />
+      </button>
+    );
+  } else return null;
 }
 
 export default CityChip;
