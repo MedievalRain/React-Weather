ffd03e9317c87615c4fa19bc5dc9c3c85a9be1ab
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-21 16:59:45 +0300

    Fix state typing

--- src/features/weather/CitiesWidget/citiesMiddleware.ts
+++ src/features/weather/CitiesWidget/citiesMiddleware.ts
@@ -1,19 +1,20 @@
 import { createListenerMiddleware } from "@reduxjs/toolkit";
 import { deleteCity, saveCity } from "../../city/citySlice";
+import { RootState } from "../../../store";
 
 const listener = createListenerMiddleware();
 
 listener.startListening({
   actionCreator: saveCity,
   effect: async (action, listenerApi) => {
-    const cities = listenerApi.getState().city.savedCities;
+    const cities = (listenerApi.getState() as RootState).city.savedCities;
     localStorage.setItem("savedCities", JSON.stringify(cities));
   },
 });
 listener.startListening({
   actionCreator: deleteCity,
   effect: async (action, listenerApi) => {
-    const cities = listenerApi.getState().city.savedCities;
+    const cities = (listenerApi.getState() as RootState).city.savedCities;
     localStorage.setItem("savedCities", JSON.stringify(cities));
   },
 });
