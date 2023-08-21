f20f08409b525a6d92b3ba0d42593f79b66de7b2
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-21 16:25:16 +0300

    Add disabled property and styling

--- src/ui/IconButton.tsx
+++ src/ui/IconButton.tsx
@@ -3,13 +3,15 @@ import { ReactNode } from "react";
 interface IconButtonProps {
   children: ReactNode;
   onClick: () => void;
+  disabled?: boolean;
 }
 
-function IconButton({ children, onClick }: IconButtonProps) {
+function IconButton({ children, onClick, disabled }: IconButtonProps) {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
-      className="flex h-8 w-8 items-center justify-center rounded-md bg-gray-700 fill-white p-1 shadow-md transition-shadow duration-200 hover:shadow-lg"
+      className="flex h-8 w-8 items-center justify-center rounded-md bg-gray-700 fill-white p-1 shadow-md transition-shadow duration-200 hover:shadow-lg disabled:cursor-not-allowed disabled:bg-gray-500"
     >
       {children}
     </button>
