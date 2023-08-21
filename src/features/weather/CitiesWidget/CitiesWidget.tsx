766c3ef428a46893a45027c5d7c728e316392358
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-21 15:46:30 +0300

    Edit mode state

--- src/features/weather/CitiesWidget/CitiesWidget.tsx
+++ src/features/weather/CitiesWidget/CitiesWidget.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { useAppSelector } from "../../../hooks/storeHooks";
 import CityChip from "./CityChip";
+import EditButton from "./EditButton";
 
 function CitiesWidget() {
   const { savedCities } = useAppSelector((state) => state.city);
+  const [isEditMode, setIsEditMode] = useState(false);
   return (
     <div className="flex flex-col gap-2 px-2 py-4">
-      <div className="font-semibold">Погода в других городах</div>
+      <div className="flex justify-between">
+        <div className="font-semibold">Погода в других городах</div>
+        <EditButton isEditMode={isEditMode} setIsEditMode={setIsEditMode} />
+      </div>
       <div className="flex flex-wrap gap-4">
         {savedCities.map((city) => (
           <CityChip key={city.id} city={city} />
