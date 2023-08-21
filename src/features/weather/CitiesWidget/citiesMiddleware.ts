759220213a0aea7f27922ce8728988abe7ebe311
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-21 16:56:38 +0300

    Delete from local storage

--- src/features/weather/CitiesWidget/citiesMiddleware.ts
+++ src/features/weather/CitiesWidget/citiesMiddleware.ts
@@ -1,10 +1,17 @@
 import { createListenerMiddleware } from "@reduxjs/toolkit";
-import { CityState, saveCity } from "../../city/citySlice";
+import { CityState, deleteCity, saveCity } from "../../city/citySlice";
 
 const listener = createListenerMiddleware();
 
 listener.startListening({
   actionCreator: saveCity,
+  effect: async (action, listenerApi) => {
+    const cities = listenerApi.getState().city.savedCities;
+    localStorage.setItem("savedCities", JSON.stringify(cities));
+  },
+});
+listener.startListening({
+  actionCreator: deleteCity,
   effect: async (action, listenerApi) => {
     const cities = listenerApi.getState().city.savedCities;
     localStorage.setItem("savedCities", JSON.stringify(cities));
