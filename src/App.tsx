66f05b5c7839647306dbc818f5fb11e8b88998a1
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-21 19:32:47 +0300

    Return Provider, cause better way to switch theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { Provider } from "react-redux";
 import Widget from "./ui/Widget";
-import store from "./store";
 import CitySearch from "./features/city/CitySearch";
 import MainWidget from "./features/weather/MainWidget/MainWidget";
 import { Suspense } from "react";
@@ -11,40 +9,45 @@ import MoonWidget from "./features/weather/MoonWidget/MoonWidget";
 import AirWidget from "./features/air/AirWidget";
 import CitiesWidget from "./features/weather/CitiesWidget/CitiesWidget";
 import Footer from "./ui/Footer";
+import { useAppSelector } from "./hooks/storeHooks";
 
 function App() {
+  const { dark } = useAppSelector((state) => state.theme);
+
   return (
     <Suspense>
-      <div className="min-h-screen w-screen flex-col bg-sky-100 fill-gray-700 text-gray-700">
+      <div
+        className={`min-h-screen w-screen flex-col bg-sky-100 fill-gray-700 text-gray-700 ${
+          "dark" && dark
+        }`}
+      >
         <main className="mx-auto flex h-full w-full max-w-5xl flex-col gap-4 p-6">
-          <Provider store={store}>
-            <CitySearch />
-            <div className="flex grid-cols-2 flex-col gap-4 md:grid">
-              <Widget>
-                <MainWidget />
-              </Widget>
-              <Widget>
-                <ForecastWidget />
-              </Widget>
-            </div>
-            <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-4">
-              <Widget>
-                <UVWidget />
-              </Widget>
-              <Widget>
-                <WindWidget />
-              </Widget>
-              <Widget>
-                <MoonWidget />
-              </Widget>
-              <Widget>
-                <AirWidget />
-              </Widget>
-            </div>
+          <CitySearch />
+          <div className="flex grid-cols-2 flex-col gap-4 md:grid">
             <Widget>
-              <CitiesWidget />
+              <MainWidget />
             </Widget>
-          </Provider>
+            <Widget>
+              <ForecastWidget />
+            </Widget>
+          </div>
+          <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-4">
+            <Widget>
+              <UVWidget />
+            </Widget>
+            <Widget>
+              <WindWidget />
+            </Widget>
+            <Widget>
+              <MoonWidget />
+            </Widget>
+            <Widget>
+              <AirWidget />
+            </Widget>
+          </div>
+          <Widget>
+            <CitiesWidget />
+          </Widget>
           <Footer />
         </main>
       </div>
