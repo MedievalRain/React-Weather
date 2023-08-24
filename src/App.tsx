b4f8d9d4f2c62dd70620272c15921ef59e6e8ad8
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-08-24 10:52:05 +0300

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,7 @@ function App() {
 
   return (
     <Suspense>
-      <div
-        className={`min-h-screen w-screen flex-col bg-sky-100 text-gray-700 dark:bg-gray-900 dark:text-gray-300 ${
-          "dark" && dark
-        }`}
-      >
+      <div className="min-h-screen w-screen flex-col bg-sky-100 text-gray-700 dark:bg-gray-900 dark:text-gray-300">
         <main className="mx-auto flex h-full w-full max-w-5xl flex-col gap-4 p-6">
           <CitySearch />
           <div className="flex grid-cols-2 flex-col gap-4 md:grid">
