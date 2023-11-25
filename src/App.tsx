7ba9f52433bad68d0422189c33f859710fd52b50
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-25 18:10:03 +0300

    Delete unused Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Widget from "./ui/Widget";
 import CitySearch from "./features/city/CitySearch";
 import MainWidget from "./features/weather/MainWidget/MainWidget";
-import { Suspense } from "react";
 import ForecastWidget from "./features/weather/ForecastWidget/ForecastWidget";
 import UVWidget from "./features/weather/UVWidget/UVWidget";
 import WindWidget from "./features/weather/WindWidget/WindWidget";
@@ -13,40 +12,38 @@ import Header from "./features/Header";
 
 function App() {
   return (
-    <Suspense>
-      <div className="min-h-screen w-screen flex-col bg-sky-100 text-gray-700 dark:bg-gray-900 dark:text-gray-300">
-        <main className="mx-auto flex h-full w-full max-w-5xl flex-col gap-4 p-6">
-          <Header />
-          <CitySearch />
-          <div className="flex grid-cols-2 flex-col gap-4 md:grid">
-            <Widget>
-              <MainWidget />
-            </Widget>
-            <Widget>
-              <ForecastWidget />
-            </Widget>
-          </div>
-          <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-4">
-            <Widget>
-              <UVWidget />
-            </Widget>
-            <Widget>
-              <WindWidget />
-            </Widget>
-            <Widget>
-              <MoonWidget />
-            </Widget>
-            <Widget>
-              <AirWidget />
-            </Widget>
-          </div>
+    <div className="min-h-screen w-screen flex-col bg-sky-100 text-gray-700 dark:bg-gray-900 dark:text-gray-300">
+      <main className="mx-auto flex h-full w-full max-w-5xl flex-col gap-4 p-6">
+        <Header />
+        <CitySearch />
+        <div className="flex grid-cols-2 flex-col gap-4 md:grid">
           <Widget>
-            <CitiesWidget />
+            <MainWidget />
           </Widget>
-          <Footer />
-        </main>
-      </div>
-    </Suspense>
+          <Widget>
+            <ForecastWidget />
+          </Widget>
+        </div>
+        <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-4">
+          <Widget>
+            <UVWidget />
+          </Widget>
+          <Widget>
+            <WindWidget />
+          </Widget>
+          <Widget>
+            <MoonWidget />
+          </Widget>
+          <Widget>
+            <AirWidget />
+          </Widget>
+        </div>
+        <Widget>
+          <CitiesWidget />
+        </Widget>
+        <Footer />
+      </main>
+    </div>
   );
 }
 
